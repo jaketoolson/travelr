effccf11fc98b43aa844d47ff0cbcf9be7f3453f
Jake Toolson <jaketoolson@gmail.com>
2018-03-03 15:29:57 -0800

    Prefix auth routes with auth

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Route::group(['prefix' => 'auth'], function(){
+    Auth::routes();
+});
 
 Route::get('/home', 'HomeController@index')->name('home');
