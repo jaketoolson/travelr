b4be4c59b4f57d5b961f25542da2d6675721f1e9
Jake Toolson <jake.toolson@fire-engine-red.com>
2018-04-06 18:40:23 -0700

    Working on custom http request class which has specific query params

---
+++ app/Http/Controllers/Requests/FieldsetsParameter.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Orion\Travelr\Http\Controllers\Requests;
+
+class FieldsetsParameter
+{
+    private $fieldsets = [];
+
+    public function addFieldset(string $resourceType, string $fieldName): void
+    {
+        $this->fieldsets[$resourceType][] = $fieldName;
+    }
+
+    public function get(): array
+    {
+        return $this->fieldsets;
+    }
+
+    public function getResourceTypes(): array
+    {
+        return array_keys($this->fieldsets);
+    }
+}
--- public/index.php
+++ public/index.php
@@ -3,13 +3,13 @@
  * Copyright (c) Jake Toolson 2018.
  */
 
-define('LARAVEL_START', microtime(true));
-
 require __DIR__.'/../vendor/autoload.php';
 $app = require_once __DIR__.'/../bootstrap/app.php';
+
+/** @var Orion\Travelr\Http\Kernel $kernel */
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 $response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
+    $request = Orion\Travelr\Http\Controllers\Requests\HttpRequest::capture()
 );
 $response->send();
 $kernel->terminate($request, $response);
