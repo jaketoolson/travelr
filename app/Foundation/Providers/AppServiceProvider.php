4fa8db83b463a13a47dbb93b5d74fdc04581d299
Jake Toolson <jake@ninjacat.io>
2020-05-23 23:34:31 -0600

    remove package

--- app/Foundation/Providers/AppServiceProvider.php
+++ app/Foundation/Providers/AppServiceProvider.php
@@ -14,11 +14,4 @@ class AppServiceProvider extends ServiceProvider
     {
         //
     }
-
-    public function register(): void
-    {
-        if ($this->app->environment() !== 'production') {
-            $this->app->register(IdeHelperServiceProvider::class);
-        }
-    }
 }
